2370cdaf77a4366d131538c3f407e91616e3f114
sanggi9900 <tkdrl9900@gmail.com>
2024-11-12 16:04:44 +0900

    20241112 -8

--- 황상기.sql
+++ 황상기.sql
@@ -4,8 +4,8 @@ use silver_button;
 SET foreign_key_checks= 1;
  -- 게시글 조건별 조회
 select * from board order by id desc ;      -- 게시글 전체 조회     
-select * from board where user_id order by id desc;   -- 게시글 작성자별 조회 id값별로 내림차순
-select * from board where title order by id desc;  --  게시글 제목별 조회 id값 별로 내림차순
+select * from board where user_id = ?order by id desc;   -- 게시글 작성자별 조회 id값별로 내림차순
+select * from board where title like '%title%' order by id desc;  --  게시글 제목별 조회 id값 별로 내림차순
 
 -- 게시글 생성 
 insert into board(user_id, title, content) values (1,'실버버튼프로젝트','실버니아');
