5b3973650e9874ed1701740a576237b5f2000410
guswl458@gmail.com <guswl458@gmail.com>
2024-11-13 16:29:03 +0900

    20241113

--- 황현지.sql
+++ 황현지.sql
@@ -1,10 +1,10 @@
 SELECT  -- 요양사/보호자/노인의 모든 쪽지 리스트 조회
-  m.id AS 메시지_ID, 
-  s.name AS 이름, 
-  r.name AS 이름, 
+  m.id AS 메시지, 
+  s.name AS 발신자,
+  r.name AS 수신자,
   m.title AS 제목, 
   m.content AS 내용, 
-  m.created_at AS 발송_시간
+  m.created_at AS 시간
 FROM message m
 JOIN users s ON m.sender_id = s.id  -- 발신자 정보
 JOIN users r ON m.receiver_id = r.id  -- 수신자 정보
@@ -13,7 +13,7 @@ ORDER BY m.created_at DESC;
 
 SELECT -- 요양사/보호자/노인의 받은 쪽지 리스트 조회
   m.id AS 메시지,
-  s.name AS 이름,
+  s.name AS 발신자,
   m.title AS 제목,
   m.content AS 내용,
   m.created_at AS 시간
@@ -25,7 +25,7 @@ ORDER BY m.created_at DESC;
 
 SELECT -- 요양사/보호자/노인의 보낸 쪽지 리스트 조회
   m.id AS 메시지,
-  s.name AS 이름,
+  r.name AS 수신자,
   m.title AS 제목,
   m.content AS 내용,
   m.created_at AS 시간
@@ -37,39 +37,26 @@ ORDER BY m.created_at DESC;
 
 SELECT * FROM users WHERE role = '요양사'; -- 모든 요양사 조회
 
- SELECT -- 특정 요양사 정보 조회
-  id AS 요양사_ID,
-  name AS 이름,
-  phone_number AS 전화번호,
-  email AS 이메일,
-  nickname AS 닉네임,
-  birth_date AS 생년월일,
-  gender AS 성별,
-  profile_image AS 프로필,
-  license_number AS 자격증번호,
-  specialization AS 전문분야
-FROM users
-WHERE role = '요양사' AND id = 2;
-
 SELECT -- 특정 쪽지 조회
-  m.id, 
-  s.name AS 발신자,
-  m.title, 
-  m.content, 
-  m.created_at
+  m.id AS 메시지, 
+  s.name AS 발신자, -- 보낸쪽지조회는 r.name 
+  m.title AS 제목, 
+  m.content AS 내용, 
+  m.created_at AS 시간
 FROM message m
 JOIN users s ON m.sender_id = s.id 
 JOIN users r ON m.receiver_id = r.id
 WHERE m.id = 1;  -- 조회하려는 쪽지 ID
 
+
 SELECT  -- 매칭 정보 조회
   m.dependent_id AS 노인_ID,
   d.name AS 노인_이름,
-  d.phone_number AS 노인_전화번호,
+  d.phone AS 노인_전화번호,
   m.caregiver_id AS 요양사_ID,
   c.name AS 요양사_이름,
-  c.phone_number AS 요양사_전화번호,
-  c.license_number AS요양사_자격증번호,
+  c.phone AS 요양사_전화번호,
+  c.license_number AS 요양사_자격증번호,
   c.specialization AS 요양사_전문분야
 FROM matchings m
 JOIN users d ON m.dependent_id = d.id
@@ -83,3 +70,17 @@ WHERE id = 5;  -- 삭제할 쪽지 ID
 -- 요양사 매칭 취소
 DELETE FROM matchings
 WHERE dependent_id = 1 AND caregiver_id = 2;
+
+SELECT -- 특정 요양사 정보 조회*******************이름변경
+  id AS 요양사,
+  name AS 이름,
+  phone AS 전화번호,
+  email AS 이메일,
+  nickname AS 닉네임,
+  birth_date AS 생년월일,
+  gender AS 성별,
+  profile_image AS 프로필,
+  license_number AS 자격증번호,
+  specialization AS 전문분야
+FROM users
+WHERE role = '요양사' AND id = 2;
